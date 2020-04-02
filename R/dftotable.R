b2f70a51815e3a8d01725f2eb8406afd99d1d593
NickCH-K <nhuntington-klein@fullerton.edu>
2020-04-01 22:31:31 -0700

    Changed version number for CRAN

--- R/dftotable.R
+++ R/dftotable.R
@@ -18,9 +18,12 @@
 #' @param row.names Flag determining whether or not the row names should be included in the table. Defaults to \code{FALSE}.
 #' @param no.escape Vector of column indices for which special characters should not be escaped (perhaps they include markup text of their own).
 #' @examples
+#'
+#' if(interactive()) {
 #' df <- data.frame(var1 = 1:4,var2=5:8,var3=c('A','B','C','D'),
 #'     var4=as.factor(c('A','B','C','C')),var5=c(TRUE,TRUE,FALSE,FALSE))
 #' dftoHTML(df,out="browser")
+#' }
 #'
 
 #' @export
